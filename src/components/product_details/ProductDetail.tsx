6f1965ecb038d2b3086ba1090c7c0c4aa0c247f2
Ged <mboyambay04@gmail.com>
2024-11-28 08:24:49 +0800

    Product detail lyout

--- src/components/product_details/ProductDetail.tsx
+++ src/components/product_details/ProductDetail.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const ProductDetail = () => {
   return (
-    <div className='border-b border-gray-200'>
+    <div className='border-t border-gray-200 py-6 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative flex flex-col lg:flex-row gap-16'>
+      <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
+        Image
+      </div>
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">DESCRIPTION</div>
 
     </div>
   )
