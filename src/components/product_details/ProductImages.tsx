e82815b6f373af53d933bf85b6c2b3ebedfb2bbf
Ged <mboyambay04@gmail.com>
2024-11-28 09:23:45 +0800

    Product Detail Layout changed

--- src/components/product_details/ProductImages.tsx
+++ src/components/product_details/ProductImages.tsx
@@ -9,7 +9,7 @@ const ProductImages = () => {
             src="https://contents.mediadecathlon.com/p2158679/k$22367151ebd5ce6d1efd9f7d09b3cc92/ultimate-disc-original-175g-white-tribord-8590276.jpg?f=768x0&format=auto"
             alt=""
             fill
-            sizes="50vw"
+            sizes="45vw"
             className="object-cover rounded-md"
             />
         </div>
