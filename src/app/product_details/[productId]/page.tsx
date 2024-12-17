215f1366514fff323083f34819a52b4e8478be5f
SydAustrei <sydaustreiserarzo2710@gmail.com>
2024-12-17 17:50:57 +0800

    pleasseee

--- src/app/product_details/[productId]/page.tsx
+++ src/app/product_details/[productId]/page.tsx
@@ -1,36 +1,33 @@
+// This file should be a Server Component by default in the App Directory
+
 import Container from '@/app/components/Container';
-import React from 'react';
 import ListRating from './ListRating';
 import { products } from '../../../../productdb/products';
 import RelatedItems from '@/app/components/RelatedItems';
 import ProductDetails from './ProductDetails';
 
-interface IParams {
-    productId: string;
-}
+// This will receive params as props automatically from Next.js
+export default async function ProductDetailsPage({ params }: { params: { productId: string } }) {
+  // Fetching product from params
+  const { productId } = params;
 
-// Mark the function as async to await params
-export default async function ProductDetailsPage({ params }: { params: IParams }) {
-    // Await params before using
-    const { productId } = await params;  // Await the params
+  // Find the product from your products array
+  const product = products.find((item) => item.id === productId);
 
-    // Find the product based on the productId from params
-    const product = products.find((item) => item.id === productId);
+  // Handle case where the product is not found
+  if (!product) {
+    return <div className="mt-28 text-center">Product not found!</div>;
+  }
 
-    // Handle product not found
-    if (!product) {
-        return <div className="mt-28 text-center">Product not found!</div>;
-    }
-
-    return (
-        <div className="mt-28">
-            <Container>
-                <ProductDetails product={product} />
-                <div className="flex flex-col mt-20 gap-4">
-                    <RelatedItems />
-                    <ListRating product={product} />
-                </div>
-            </Container>
+  return (
+    <div className="mt-28">
+      <Container>
+        <ProductDetails product={product} />
+        <div className="flex flex-col mt-20 gap-4">
+          <RelatedItems />
+          <ListRating product={product} />
         </div>
-    );
+      </Container>
+    </div>
+  );
 }
